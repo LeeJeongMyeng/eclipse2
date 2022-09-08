4e7928ec3aac6c4c1f89deb5cf61331a26bbf7ee
aoddl <aoddl@DESKTOP-TUACU4S>
2022-09-08 14:38:50 +0900

    test

--- javaexp/src/a06_object_objectReview/A03_MethodsExp.java
+++ javaexp/src/a06_object_objectReview/A03_MethodsExp.java
@@ -11,7 +11,18 @@ public class A03_MethodsExp {
 		m1.buyProd(30);
 		m1.buyProd(30);
 		m1.buyProd(30);
-		System.out.println(m1.getProdCnt());
+		System.out.println("총 갯수 : "+m1.getProdCnt());
+		Mart m2= new Mart();
+		m2.buyprodCnt("사과");
+		m2.buyprodCnt("바나나");
+		m2.buyprodCnt("딸기");
+		System.out.println("총 갯수 : "+m2.getProdCnt2());
+		m2.buyProd("컴퓨터");
+		m2.buyProd("컴퓨터");
+		m2.buyProd("컴퓨터");
+		m2.buyProd("컴퓨터");
+		m2.buyProd("컴퓨터");
+		m2.showList();
 	}
 
 }
@@ -19,13 +30,44 @@ public class A03_MethodsExp {
 	class Mart{
 		int count;
 		int totcount;
+		String pnames;
+		String totpname;
+		String buylist[];
+		int buyIdx;
+		Mart(){
+			this.pnames="";  //이거 안붙히면 앞에 null이 불음
+			buylist = new String[5]; //배열 선언
+		}
+		void buyProd(String pname) {
+			if(buyIdx<5) {
+				buylist[buyIdx++]=pnames;
+			}else {
+				System.out.println("구매물건은 총 5개까지 가능합니다");
+			}
+			
+		}
 		
+		void showList() {
+			for(int idx=0;idx<buylist.length;idx++) {
+				System.out.println(idx+1+")"+buylist[idx]);
+			}
+		}
 		void buyProd(int count) {
 			this.totcount+=count; // 누적저장
 			System.out.println("현재까지 저장된 카운트:"+totcount);
 		}
+		void buyprodCnt(String pname) {
+			this.pnames=pname;
+			
+		}
+		
 		int getProdCnt() {
 			return this.totcount;
+			
+		}
+		String getProdCnt2() {
+			return this.pnames+=pnames+",";
+			
 		}
 	}
 	
